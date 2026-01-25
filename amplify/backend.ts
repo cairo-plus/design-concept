3b78ed2840e74bdc606c394f9468d3f26080b7ca
msato.umemoto@woven-planet.global <msato.umemoto@woven-planet.global>
2026-01-25 14:03:30 +0900

    fix: revert to amplify convention and ensure tsconfig includes files

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,21 +1,5 @@
-import { defineBackend, defineAuth } from '@aws-amplify/backend';
-
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-const auth = defineAuth({
-    loginWith: {
-        email: true,
-    },
-    userAttributes: {
-        preferredUsername: {
-            mutable: true,
-            required: false,
-        },
-    },
-    accountRecovery: 'EMAIL_ONLY',
-});
+import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
 
 /**
  * @see https://docs.amplify.aws/gen2/build-a-backend/
