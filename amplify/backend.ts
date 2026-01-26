cb3de2a0b8ec2cfc5e86ddfb4fabe8e65fc7f0e9
msato.umemoto@woven-planet.global <msato.umemoto@woven-planet.global>
2026-01-26 18:15:55 +0900

    fix: flatten auth config access to resolve CI import error

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/config';
+import { auth } from './auth_resource';
 import { storage } from './storage/resource';
 
 
