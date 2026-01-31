07fad85dba72535b33238085c179b7ccacf94c6f
msato.umemoto@woven-planet.global <msato.umemoto@woven-planet.global>
2026-01-31 21:09:52 +0900

    fix(amplify): rename auth resource to avoid import resolution issues

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
+import { auth } from './auth/auth-res';
 import { storage } from './storage/resource';
 import { data } from './data/resource';
 import { processBedrock } from './functions/process-bedrock/resource';
